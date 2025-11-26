861927823face98a809a38aa785f7d89976549a0
squidwonk-afk <artfallsdown@gmail.com>
2025-11-26 19:20:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
-  base: "/", 
+  base: "/artfall/", // IMPORTANT for GitHub Pages
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
     outDir: "dist",
-    target: "esnext",
+    emptyOutDir: true,
   },
 });
