2f2877f3688477eac20c2bd4151de87a868e9be4
squidwonk-afk <artfallsdown@gmail.com>
2025-11-27 07:58:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: '/',   // for custom domain
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    target: 'esnext',
-  },
-})
+  base: "/artfall/", // repo name
+});
